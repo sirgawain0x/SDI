9887fb40d3bf8801b867cf3da62ee08377b96a5f
Gawain Bracy II <g2musicgroup@fullsail.edu>
2012-05-02 00:53:51 -0400

    added contents to new repo.
    Signed-off-by: Gawain Bracy II <g2musicgroup@fullsail.edu>

---
+++ CollegeSelector/CollegeSelector/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CollegeSelector
+//
+//  Created by Gawain Bracy II on 3/29/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CollegeSelector/CollegeSelector/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  CollegeSelector
+//
+//  Created by Gawain Bracy II on 3/29/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
